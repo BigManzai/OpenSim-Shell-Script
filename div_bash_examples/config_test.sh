2e10501cdbc22a77458ec745852085add54a2054
BigManzai <39168019+BigManzai@users.noreply.github.com>
2022-08-04 18:52:21 +0200

    Add files via upload

--- div_bash_examples/config_test.sh
+++ div_bash_examples/config_test.sh
@@ -2,23 +2,32 @@
 
 # Konfigurationen so abaendern das sie maschinell weiterverarbeitet werden koennen.
 
-datei=$1
 
-#datei="OpenSim.ini"
+    datei=$1
 
-#echo "Loesche $datei"
-#rm $datei
+    echo "Loesche $datei.ini.cnf"
+    rm "$datei.ini.cnf"
 
-echo "Loesche alle Zeilen mit ;"
-#sed -i -e '/string/d' input
-sed -i -e '/;/d' "$datei" #OK geht
+    echo "Kopiere $datei.ini nach $datei.ini.cnf"
+    cp "$datei.ini" "$datei.ini.cnf"
 
-echo "Fuehrende Leerzeichen (Leerzeichen, Tabulatoren) vor jeder Zeile loeschen"
-sed -i -e 's/^[ \t]*//' "$datei"
+    echo "Loesche alle Zeilen mit ;"
+    #sed -i -e '/string/d' input
+    sed -i -e '/;/d' "$datei.ini.cnf"
 
-echo "Loesche alle Zeilen mit Leerstellen"
-sed -i -e '/^$/d' "$datei"
+    echo "Fuehrende Leerzeichen (Leerzeichen, Tabulatoren) vor jeder Zeile loeschen"
+    sed -i -e 's/^[ \t]*//' "$datei.ini.cnf"
 
-echo "Einstrich Sonderzeichen an den anfang und ende jeder Zeile haengen."
-sed -i -e s/^/\'/ "$datei"
-sed -i -e s/$/\'/g "$datei"
+    echo "Loesche alle Zeilen mit Leerstellen"
+    sed -i -e '/^$/d' "$datei.ini.cnf"
+
+    echo "Ersetze alle doppelten Hochstriche gegen einfache."
+    sed -i -e s/\"/\'/g "$datei.ini.cnf"
+
+    echo "Einstrich Sonderzeichen an den anfang und ende jeder Zeile haengen."
+    sed -i -e s/^/\"/ "$datei.ini.cnf"
+    sed -i -e s/$/\"/g "$datei.ini.cnf"
+
+    echo "Ein Array daraus machen."
+    sed -i -e 1 i\$dateiConfigList=\( "$datei.ini.cnf"
+    sed -i -e '$a)' "$datei.ini.cnf"
