94ab8f083cb2974f085e7d66099bcaf1cd01f740
BigManzai <39168019+BigManzai@users.noreply.github.com>
2022-09-18 01:18:13 +0200

    collection

--- osmt_web/mit_parameter.php
+++ osmt_web/mit_parameter.php
@@ -6,6 +6,135 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <body>
 
+<?php
+// Funktion zum aufruf von opensim.sh Funktionen mit mehreren Parametern.
+function commandaufruf2(string $funktionsname, string $parameter2)
+{
+    $ausgabe=null; $rueckgabewert=null;
+	$datei="/opt/opensim.sh";
+    $zusammengesetzt="$datei $funktionsname $parameter2";
+    exec($zusammengesetzt, $ausgabe, $rueckgabewert);
+
+	?> 	<div class="alert">
+		<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+	<?php 
+
+    foreach ($ausgabe as $bildschirmausgabe)
+    {
+		echo "<li>".$bildschirmausgabe."</li>";		
+    }
+
+	?> </div> <?php
+}
+function commandaufruf3(string $funktionsname, string $parameter2, string $parameter3)
+{
+    $ausgabe=null; $rueckgabewert=null;
+	$datei="/opt/opensim.sh";
+    $zusammengesetzt="$datei $funktionsname $parameter2 $parameter3";
+    exec($zusammengesetzt, $ausgabe, $rueckgabewert);
+
+	?> 	<div class="alert">
+		<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+	<?php 
+
+    foreach ($ausgabe as $bildschirmausgabe)
+    {
+		echo "<li>".$bildschirmausgabe."</li>";		
+    }
+
+	?> </div> <?php
+}
+function commandaufruf4(string $funktionsname, string $parameter2, string $parameter3, string $parameter4)
+{
+    $ausgabe=null; $rueckgabewert=null;
+	$datei="/opt/opensim.sh";
+    $zusammengesetzt="$datei $funktionsname $parameter2 $parameter3 $parameter4";
+    exec($zusammengesetzt, $ausgabe, $rueckgabewert);
+
+	?> 	<div class="alert">
+		<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+	<?php 
+
+    foreach ($ausgabe as $bildschirmausgabe)
+    {
+		echo "<li>".$bildschirmausgabe."</li>";		
+    }
+
+	?> </div> <?php
+}
+function commandaufruf5(string $funktionsname, string $parameter2, string $parameter3, string $parameter4, string $parameter5)
+{
+    $ausgabe=null; $rueckgabewert=null;
+	$datei="/opt/opensim.sh";
+    $zusammengesetzt="$datei $funktionsname $parameter2 $parameter3 $parameter4 $parameter5";
+    exec($zusammengesetzt, $ausgabe, $rueckgabewert);
+
+	?> 	<div class="alert">
+		<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+	<?php 
+
+    foreach ($ausgabe as $bildschirmausgabe)
+    {
+		echo "<li>".$bildschirmausgabe."</li>";		
+    }
+
+	?> </div> <?php
+}
+function commandaufruf6(string $funktionsname, string $parameter2, string $parameter3, string $parameter4, string $parameter5, string $parameter6)
+{
+    $ausgabe=null; $rueckgabewert=null;
+	$datei="/opt/opensim.sh";
+    $zusammengesetzt="$datei $funktionsname $parameter2 $parameter3 $parameter4 $parameter5 $parameter6";
+    exec($zusammengesetzt, $ausgabe, $rueckgabewert);
+
+	?> 	<div class="alert">
+		<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+	<?php 
+
+    foreach ($ausgabe as $bildschirmausgabe)
+    {
+		echo "<li>".$bildschirmausgabe."</li>";		
+    }
+
+	?> </div> <?php
+}
+function commandaufruf7(string $funktionsname, string $parameter2, string $parameter3, string $parameter4, string $parameter5, string $parameter6, string $parameter7)
+{
+    $ausgabe=null; $rueckgabewert=null;
+	$datei="/opt/opensim.sh";
+    $zusammengesetzt="$datei $funktionsname $parameter2 $parameter3 $parameter4 $parameter5 $parameter6 $parameter7";
+    exec($zusammengesetzt, $ausgabe, $rueckgabewert);
+
+	?> 	<div class="alert">
+		<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+	<?php 
+
+    foreach ($ausgabe as $bildschirmausgabe)
+    {
+		echo "<li>".$bildschirmausgabe."</li>";		
+    }
+
+	?> </div> <?php
+}
+function commandaufruf8(string $funktionsname, string $parameter2, string $parameter3, string $parameter4, string $parameter5, string $parameter6, string $parameter7, string $parameter8)
+{
+    $ausgabe=null; $rueckgabewert=null;
+	$datei="/opt/opensim.sh";
+    $zusammengesetzt="$datei $funktionsname $parameter2 $parameter3 $parameter4 $parameter5 $parameter6 $parameter7 $parameter8";
+    exec($zusammengesetzt, $ausgabe, $rueckgabewert);
+
+	?> 	<div class="alert">
+		<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+	<?php 
+
+    foreach ($ausgabe as $bildschirmausgabe)
+    {
+		echo "<li>".$bildschirmausgabe."</li>";		
+    }
+
+	?> </div> <?php
+}
+?>
 
 <?php
 //1
