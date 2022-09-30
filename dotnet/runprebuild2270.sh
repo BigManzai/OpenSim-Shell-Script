1aef33f3f96ed2de101eefdcfcd377d210a45205
BigManzai <39168019+BigManzai@users.noreply.github.com>
2022-10-01 01:51:57 +0200

    Add files via upload

---
+++ dotnet/runprebuild2270.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+case "$1" in
+
+  'clean')
+
+    mono bin/Prebuild.exe /clean
+
+  ;;
+
+
+  'autoclean')
+
+    echo y|mono bin/Prebuild.exe /clean
+
+  ;;
+
+
+
+  *)
+
+    mono bin/Prebuild.exe /target vs2022 /targetframework net7_0 /excludedir = "obj | bin"
+
+  ;;
+
+esac
