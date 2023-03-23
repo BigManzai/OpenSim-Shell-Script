15a873f7ef607eabce1d1d0a9aec05c8560440e9
BigManzai <39168019+BigManzai@users.noreply.github.com>
2023-03-23 08:57:00 +0100

    Add files via upload

---
+++ div_bash_examples/bashtest.php
@@ -0,0 +1,7 @@
+
+<?php
+//  Die Skripte phptest.sh und bashtest.php gehoeren zusammen.
+echo "<h1>Dies ist dies ist ein PHP Skript und gehört in das html Verzeichnis.</h1>";
+$output = shell_exec('/opt/phptest.sh');
+echo "<pre><h1>$output</h1></pre>";
+?>
---
+++ div_bash_examples/phptest.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Die Skripte phptest.sh und bashtest.php gehoeren zusammen.
+echo "Dies ist ein Bash Skript und gehört in ein Verzeichnis wie /opt oder /home."
