da6a9bde0c2cfffb05c5caaf1037370dee626ce9
BigManzai <39168019+BigManzai@users.noreply.github.com>
2023-09-15 12:41:20 +0200

    Experimental Funktionen zu OS Banking

---
+++ web/helper/bankingExperimental.php
@@ -0,0 +1,342 @@
+<?php
+// Datenbankverbindung herstellen
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$dbname = "MoneyDatabase";
+$receiverID = "123456789";
+$UUID = "123456789";
+
+// Fehlerprotokoll auf dem Bildschirm aktivieren
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Fehlerprotokoll in eine Datei schreiben
+ini_set('log_errors', 1);
+ini_set('error_log', 'moneyerror.log');
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
+}
+
+// Funktion, um das Guthaben eines Benutzers abzurufen
+function getBalance($userID) {
+    global $conn;
+    $query = "SELECT balance FROM balances WHERE user = ?";
+    
+    if ($stmt = $conn->prepare($query)) {
+        $stmt->bind_param("s", $userID);
+        $stmt->execute();
+        $stmt->bind_result($balance);
+        $stmt->fetch();
+        $stmt->close();
+        return $balance;
+    } else {
+        die("Fehler beim Abfragen des Guthabens: " . $conn->error);
+    }
+}
+
+// Funktion, um das Guthaben eines Benutzers zu aktualisieren
+function updateBalance($userID, $amount) {
+    global $conn;
+    $query = "UPDATE balances SET balance = balance + ? WHERE user = ?";
+    
+    if ($stmt = $conn->prepare($query)) {
+        $stmt->bind_param("is", $amount, $userID);
+        $stmt->execute();
+        $stmt->close();
+    } else {
+        die("Fehler beim Aktualisieren des Guthabens: " . $conn->error);
+    }
+}
+
+// Funktion, um Geld an einen anderen Benutzer zu überweisen
+function giveMoney($UUID, $receiverID, $amount) {
+    global $conn;
+    $conn->autocommit(false); // Transaktion starten
+    
+    // Geld vom Sender abziehen
+    updateBalance($UUID, -$amount);
+    
+    // Geld zum Empfänger hinzufügen
+    updateBalance($receiverID, $amount);
+    
+    // Transaktion hinzufügen
+    $transactionData = [
+        'UUID' => $UUID,
+        'senderID' => $UUID,
+        'receiverID' => $receiverID,
+        'amount' => $amount,
+        'status' => 1, // 1 für erfolgreich, 0 für ausstehend oder fehlgeschlagen
+        'description' => 'Überweisung erfolgreich'
+    ];
+    addTransaction($transactionData);
+    
+    // Transaktion bestätigen
+    $conn->commit();
+    $conn->autocommit(true);
+}
+
+// Funktion, um eine Transaktion hinzuzufügen
+function addTransaction($transactionData) {
+    global $conn;
+    $query = "INSERT INTO transactions (UUID, sender, receiver, amount, status, description) VALUES (?, ?, ?, ?, ?, ?)";
+    
+    if ($stmt = $conn->prepare($query)) {
+        $stmt->bind_param("sssiis", $transactionData['UUID'], $transactionData['sender'], $transactionData['receiver'], $transactionData['amount'], $transactionData['status'], $transactionData['description']);
+        //$stmt->execute();
+        $stmt->close();
+    } else {
+        die("Fehler beim Hinzufügen der Transaktion: " . $conn->error);
+    }
+}
+
+// Funktion, um den Status einer Transaktion zu aktualisieren
+function updateTransactionStatus($UUID, $status, $description) {
+    global $conn;
+    $query = "UPDATE transactions SET status = ?, description = ? WHERE UUID = ?";
+    
+    if ($stmt = $conn->prepare($query)) {
+        $stmt->bind_param("iss", $status, $description, $UUID);
+        $stmt->execute();
+        $stmt->close();
+    } else {
+        die("Fehler beim Aktualisieren des Transaktionsstatus: " . $conn->error);
+    }
+}
+
+// Test
+
+// Function to get balance status
+function getBalanceStatus($userID) {
+    // Assuming you have a table named 'balances' with a column 'status'
+    global $conn;
+    $query = "SELECT status FROM balances WHERE user = ?";
+    
+    if ($stmt = $conn->prepare($query)) {
+        $stmt->bind_param("s", $userID);
+        $stmt->execute();
+        $stmt->bind_result($status);
+        $stmt->fetch();
+        $stmt->close();
+        return $status;
+    } else {
+        die("Fehler beim Abfragen des Balance-Status: " . $conn->error);
+    }
+}
+
+// Function to update balance status
+function updateBalanceStatus($userID, $status) {
+    global $conn;
+    $query = "UPDATE balances SET status = ? WHERE user = ?";
+    
+    if ($stmt = $conn->prepare($query)) {
+        $stmt->bind_param("is", $status, $userID);
+        $stmt->execute();
+        $stmt->close();
+    } else {
+        die("Fehler beim Aktualisieren des Balance-Status: " . $conn->error);
+    }
+}
+
+// Function to withdraw money
+function withdrawMoney($transactionID, $senderID, $amount) {
+    // Assuming you have a table for transactions where you record withdrawals
+    // and a table for balances to deduct the amount from the sender's balance
+    global $conn;
+    $conn->autocommit(false); // Start transaction
+    
+    // Deduct the amount from the sender's balance
+    updateBalance($senderID, -$amount);
+    
+    // Add the transaction record
+    $transactionData = [
+        'UUID' => $transactionID,
+        'senderID' => $senderID,
+        'receiverID' => null, // You may update this depending on your transaction structure
+        'amount' => $amount,
+        'status' => 1, // Assuming 1 means successful withdrawal
+        'description' => 'Withdrawal successful'
+    ];
+    addTransaction($transactionData);
+    
+    // Commit the transaction
+    $conn->commit();
+    $conn->autocommit(true);
+}
+
+// Function to fetch transaction UUID
+function FetchTransactionUUID($transactionID) {
+    // Assuming you have a table for transactions with UUIDs
+    global $conn;
+    $query = "SELECT UUID FROM transactions WHERE UUID = ?";
+    
+    if ($stmt = $conn->prepare($query)) {
+        $stmt->bind_param("s", $transactionID);
+        $stmt->execute();
+        $stmt->bind_result($uuid);
+        $stmt->fetch();
+        $stmt->close();
+        return $uuid;
+    } else {
+        die("Fehler beim Abrufen der Transaktions-UUID: " . $conn->error);
+    }
+}
+
+// Test Ende
+
+// Überprüfen, ob das Formular gesendet wurde
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userID = $_POST["userID"];
+    $amount = $_POST["amount"];
+    
+    // Überprüfe, welche Checkboxen ausgewählt wurden und rufe die entsprechenden Funktionen auf
+    if (isset($_POST["giveMoney"])) {
+        // Implementiere diese Funktion entsprechend deiner Anforderungen
+        // Füge die Ergebnisse der Abfrage in eine Variable ein, die du später ausgeben kannst
+        giveMoney($UUID, $receiverID, $amount);
+    }
+    // Überprüfe, welche Checkboxen ausgewählt wurden und rufe die entsprechenden Funktionen auf
+    if (isset($_POST["FetchTransactionUUID"])) {
+        // Funktionsaufruf für FetchTransaction
+        // Implementiere diese Funktion entsprechend deiner Anforderungen
+        // Füge die Ergebnisse der Abfrage in eine Variable ein, die du später ausgeben kannst
+        $fetchTransactionResult = FetchTransactionUUID($transactionID);
+    }
+
+    if (isset($_POST["getBalance"])) {
+        // Implementiere diese Funktion entsprechend deiner Anforderungen
+        // Füge die Ergebnisse der Abfrage in eine Variable ein, die du später ausgeben kannst
+        $getBalanceResult = getBalance($userID);
+    }
+
+    if (isset($_POST["updateBalance"])) {
+        // Funktionsaufruf für SetTransExpired
+        // Implementiere diese Funktion entsprechend deiner Anforderungen
+        // Füge die Ergebnisse der Abfrage in eine Variable ein, die du später ausgeben kannst
+        $updateBalanceResult = updateBalance($userID, $amount);
+    }
+
+    if (isset($_POST["addTransaction"])) {
+        // Implementiere diese Funktion entsprechend deiner Anforderungen
+        // Füge die Ergebnisse der Abfrage in eine Variable ein, die du später ausgeben kannst
+        $addTransactionResult = addTransaction($transactionData);
+    }
+
+    if (isset($_POST["updateTransactionStatus"])) {
+        // Implementiere diese Funktion entsprechend deiner Anforderungen
+        // Füge die Ergebnisse der Abfrage in eine Variable ein, die du später ausgeben kannst
+        $updateTransactionStatusResult = updateTransactionStatus($UUID, $status, $description);
+    }
+
+    if (isset($_POST["getBalanceStatus"])) {
+        // Implementiere diese Funktion entsprechend deiner Anforderungen
+        // Füge die Ergebnisse der Abfrage in eine Variable ein, die du später ausgeben kannst
+        $getBalanceStatusResult = getBalanceStatus($userID);
+    }
+    if (isset($_POST["updateBalanceStatus"])) {
+        // Implementiere diese Funktion entsprechend deiner Anforderungen
+        // Füge die Ergebnisse der Abfrage in eine Variable ein, die du später ausgeben kannst
+        $updateBalanceStatusResult = updateBalanceStatus($userID, $status);
+    }
+    if (isset($_POST["withdrawMoney"])) {
+        // Implementiere diese Funktion entsprechend deiner Anforderungen
+        // Füge die Ergebnisse der Abfrage in eine Variable ein, die du später ausgeben kannst
+        $withdrawMoneyResult = withdrawMoney($transactionID, $senderID, $amount);
+    }
+
+    $balance = getBalance($userID);
+    echo "<br>Guthaben von Benutzer $userID:<br> $balance<br>";
+
+    updateBalance($userID, $amount);
+    echo "<br>Guthaben von Benutzer $userID <br> nach der Aktualisierung: <br>" . getBalance($userID) . "<br>";
+
+    $transactionStatus = 1; // 1 für erfolgreich, 0 für ausstehend oder fehlgeschlagen
+    $transactionDescription = "Überweisung erfolgreich";
+    updateTransactionStatus($UUID, $transactionStatus, $transactionDescription);
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html>
+<style>
+input[type=text], select {
+  width: 95%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=checkbox] {
+  margin-right: 5px;
+}
+
+input[type=submit] {
+  width: 95%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+div {
+  width: 25%;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+</style>
+<head>
+    <title>OpenSim Banking</title>
+    <h1>OpenSim Banking</h1>
+</head>
+<body>
+    <div>
+    <h2>Geldüberweisung Spielwiese Experiment</h2>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        
+        <input type="checkbox" name="giveMoney" id="giveMoney" checked="checked">
+        <label for="giveMoney">Geld an User</label><br>
+        
+        <input type="checkbox" name="FetchTransactionUUID" id="FetchTransactionUUID">
+        <label for="FetchTransactionUUID">Transaktions-UUID abrufen</label><br>
+        
+        <input type="checkbox" name="getBalance" id="getBalance">
+        <label for="getBalance">Kontostand abrufen</label><br>
+        
+        <input type="checkbox" name="updateBalance" id="updateBalance">
+        <label for="updateBalance">Aktualisieren des Kontostandes</label><br>
+        
+        <input type="checkbox" name="addTransaction" id="addTransaction">
+        <label for="addTransaction">Transaktion hinzufügen</label><br>
+        
+        <input type="checkbox" name="getBalanceStatus" id="getBalanceStatus">
+        <label for="agetBalanceStatus">Kontostand holen</label><br>
+        
+        <input type="checkbox" name="updateBalanceStatus" id="updateBalanceStatus">
+        <label for="updateBalanceStatus">Kontostand Aktualisieren</label><br>
+        
+        <input type="checkbox" name="withdrawMoney" id="withdrawMoney">
+        <label for="withdrawMoney">Geld abheben</label><br><br><br>
+
+        Benutzer-ID: <br><input type="text" name="userID" id="userID"><br><br>
+        Betrag: <br><input type="text" name="amount" id="amount"><br><br>
+        <input type="submit" value="Anwenden">
+    </form>
+</div>
+</body>
+</html>
