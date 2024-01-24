2d5fb5f8044eea050b5419dd44fd1814ef3c56d5
BigManzai <39168019+BigManzai@users.noreply.github.com>
2024-01-24 11:31:17 +0100

    Add files via upload

---
+++ Python/python-vbit2.py
@@ -0,0 +1,25 @@
+import struct
+import subprocess
+
+def getch():
+    print("Python version:")
+    subprocess.run(["python", "--version"])
+
+    if struct.calcsize("P") * 8 == 64:
+        print("Running in 64-bit mode.")
+    else:
+        print("Running in 32-bit mode.")
+
+    print("\nOther versions:")
+    subprocess.run(["cmake", "--version"])
+    subprocess.run(["git", "--version"])
+    subprocess.run(["pip", "--version"])
+    
+    #subprocess.run(["pip", "install", "llbase"])
+    #subprocess.run(["pip", "install", "git+https://github.com/secondlife/autobuild.git#egg=autobuild"])
+    #subprocess.run(["pip", "install", "llsd"])
+
+    #print("\nCloning Firestorm repository:")
+    #subprocess.run(["git", "clone", "https://github.com/FirestormViewer/phoenix-firestorm-alpha"])
+
+getch()
